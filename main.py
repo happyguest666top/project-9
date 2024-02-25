624ee4c25fffc1f3ab8f6bff80b5f53a0107c5e0
happyguest666top <145270176+happyguest666top@users.noreply.github.com>
2024-02-25 17:33:35 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from pygame import *
+
+window = display.set_mode((700, 500))
+display.set_caption('Доганялки')
+background = transform.scale(image.load('бикини ботом.jpg'), (700, 500))
+clock = time.Clock()
+FPS = 60
+run = True
+x1 = 100
+y1 = 350
+sprite1 = transform.scale(image.load('патрик-removebg-preview (1).png'), (100,100))
+x2 = 400
+y2 = 350
+sprite2 = transform.scale(image.load('губка боб.png'), (100,100))
+while run:
+    window.blit(background, (0,0))
+    window.blit(sprite1, (x1, y1))
+    window.blit(sprite2, (x2, y2))
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    keys_pressed = key.get_pressed()
+    if keys_pressed[K_LEFT] and x1>5:
+        x1 -= 10
+    if keys_pressed[K_RIGHT] and x1 < 595:
+        x1 += 10
+    if keys_pressed[K_UP] and y1 > 5:
+        y1 -= 10
+    if keys_pressed[K_DOWN] and y1 < 395:
+        y1 += 10
+    if keys_pressed[K_a] and x2>5:
+        x2 -= 10
+    if keys_pressed[K_d] and x2 < 595:
+        x2 += 10
+    if keys_pressed[K_w] and y2 > 5:
+        y2 -= 10
+    if keys_pressed[K_s] and y2 < 395:
+        y2 += 10
+    display.update()
+    clock.tick(FPS)
